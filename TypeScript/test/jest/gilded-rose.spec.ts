847aeadf6d79b929eca6031065433ece2963f08f
O'keefe <brendan.okeefe@charter.com>
2023-09-12 07:31:37 -0600

    initial solve

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,14 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe("Gilded Rose", () => {
+  it('should decrement quality of a "normal" item', () => {
+    const gildedRose = new GildedRose([new Item("normal", 7, 20)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].quality).toBe(19);
+  });
+  it('should decrement quality of a "Conjured" item', () => {
+    const gildedRose = new GildedRose([new Item("Conjured", 7, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(18);
   });
 });
